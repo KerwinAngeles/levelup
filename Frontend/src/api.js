cf8cdef362c6be520acd814309037799fe6070b9
KerwinAngeles <kerwinangeles12@gmail.com>
2025-07-07 02:08:33 +0200

    [Fead]: Change url

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: process.env.NODE_ENV === 'production'
-    ? 'https://tu-dominio.com'
+    ? 'https://levelup-production-96c2.up.railway.app'
     : 'http://localhost:5000',  
   withCredentials: true         
 });
